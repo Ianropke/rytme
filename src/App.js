e36fc7a7f93806eba6f382a6058b7b19d08137a7
Ianropke <ianropke@gmail.com>
2024-12-26 15:45:17 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import "./App.css";
 import React from "react";
-import RhythmGame from "./components/RhythmGame";
+import RhythmGame from "./components/RhythmGame"; // Korrekt import af RhythmGame-komponenten
+import "./App.css"; // Import af App.css for styling
 
 function App() {
-  return <RhythmGame />;
+  return (
+    <div className="App">
+      <RhythmGame />
+    </div>
+  );
 }
 
 export default App;
