23e587c0113a1980030c77701c9a464e2dd29962
Ianropke <ianropke@gmail.com>
2024-12-26 17:09:46 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import RhythmGameWithMusic from "./components/RhythmGame"; // Brug RhythmGame, som inkluderer musikken
-import "./App.css"; // Styling
+import RhythmGame from "./components/RhythmGame";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Rhythm Game</h1>
-        <h2>Play along with the beat of "beatcheck.mp3"</h2>
+        <h2>Follow the beat of "beatcheck.mp3"</h2>
       </header>
       <main>
-        <RhythmGameWithMusic />
+        <RhythmGame />
       </main>
     </div>
   );
